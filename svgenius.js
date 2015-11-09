0ea51d1c72bd5e8c6c3758dda8f0eb6a31f5a3d2
Joe Wynne <josephryanwynne@gmail.com>
2015-11-09 22:59:56 +0000

    Pulled out some logic into functions to reduce repetition and added a base layer so the arc background can be a different color (white) to the svg background

--- svgenius.js
+++ svgenius.js
@@ -8,7 +8,6 @@ TODO
 ====
 
 * Add labels: Chart Title, Chart Major, Chart Minor. Title is least important.
-* Add 0 point label
 * Changing the stroke width screws up the layout
 * Don't forget to remove logging once it's working ok
 * setInterval to make value gradually fill up?
@@ -20,92 +19,80 @@ SVGENIUS.charts = {
     newSvgElement: function(tag){
         return document.createElementNS("http://www.w3.org/2000/svg", tag);
     },
-    
-    
-    
-    gauge: function(conf){
-        
-        var gaugeContainer = document.getElementById(conf.targetContainerId);
-        
-        var height = gaugeContainer.getAttribute("height");
-        var width = gaugeContainer.getAttribute("width"); //Not using width for much except creating the svg
-        
-        
-        var strokeWidth = 1;
-        
+    newSvg: function(height, width){
         var svg = this.newSvgElement('svg');
         svg.setAttribute('xmlns:svg',"http://www.w3.org/2000/svg");
         svg.setAttribute('xmlns', "http://www.w3.org/2000/svg");
         svg.setAttribute('height', height);
         svg.setAttribute('width', width);
+        return svg;
+    },
+    svgText: function(xPos,yPos,textSize,text,color){
+        var newSvgTextElement = this.newSvgElement('text');
+        newSvgTextElement.setAttribute('alignment-baseline', 'middle');
+        newSvgTextElement.setAttribute('x', xPos);
+        newSvgTextElement.setAttribute('y', yPos);
+        newSvgTextElement.setAttribute('font-size', textSize);
+        newSvgTextElement.setAttribute('font-weight', 'bold');
+        newSvgTextElement.setAttribute('fill', color);
+        newSvgTextElement.innerHTML = text;
+        newSvgTextElement.setAttribute('font-family', 'Arial');
+        return newSvgTextElement;
+    },
+    gauge: function(conf){
         
+        var gaugeContainer = document.getElementById(conf.targetContainerId);
         
-        var gaugeWidth = height/12; //Arbitrary choice made by me. Looks like a reasonable ratio
-        var radius = height / 2.0 - 5;
+        //This is probably not the right way to ascertain the dimensions as it only works for html attributes
+        var height = gaugeContainer.getAttribute("height"); 
+        var width = gaugeContainer.getAttribute("width");
+        var strokeWidth = 1; // TODO : Bug in my code means this cannot easily be changed.
+        var svg = this.newSvg(height, width);
+        //Compute positions of various stuff based on the above
+        var gaugeWidth = height/12; //Arbitrary ratio choice made by me. Looks ok
+        var radius = (height - 2) / 2.0; // Allow 1 pixel either side for padding
         var innerRadius = radius - gaugeWidth;
-        
-        
         var startX = radius * 2/3;
         var startY = gaugeWidth+1; //This is a bad idea. Use a central point as a starting point instead? 
-        
-        //Grey outline of the gauge - draw vertical line up, outer arc, up, inner arc.
-        var outline = this.newSvgElement("path");
         var outerArcBottom = (2*radius);
-        outline.setAttribute('d', 'M'+startX+','+startY+' v-' + gaugeWidth + ' A1,1 0 0 1 '+startX+' '+outerArcBottom+' v-'+gaugeWidth+' A1,1 0 0 0 '+startX+','+startY+' Z');
-        outline.setAttribute('fill', 'none');
-        outline.setAttribute('stroke', 'grey');
-        outline.setAttribute('stroke-width', '1');
-        
-        //Compute text positions
-        var textSize = gaugeWidth;
         var halfwayX = (startX+radius-(strokeWidth/2));
         var halfwayY = (radius+1);
         var halfwayPoint = halfwayX+','+halfwayY; // Check that this is correct
         
+        //Grey outline of the gauge - draw vertical line up, outer arc, up, inner arc.
+        var outline = this.newSvgElement("path");
+        var basePath = 'M'+startX+','+startY+' v-' + gaugeWidth + ' A1,1 0 0 1 '+startX+' '+outerArcBottom+' v-'+gaugeWidth+' A1,1 0 0 0 '+startX+','+startY+' Z';
+        outline.setAttribute('d', basePath);
+        outline.setAttribute('fill', 'none');
+        outline.setAttribute('stroke', 'grey');
+        outline.setAttribute('stroke-width', '1');
+        //Separate layer for base and outline so colors can be manipulated appropriately
+        var base = this.newSvgElement("path");
+        base.setAttribute('d', basePath);
+        base.setAttribute('fill', 'white');
+        
+        //Compute text positions
+        var textSize = gaugeWidth;
         var positiveTextPositionX = startX - gaugeWidth;
-        var positiveTextPositionY = startY - (textSize / 4 ); //Y-position is the bottom of the text
+        var positiveTextPositionY = startY - (textSize / 2);
         
-        var positiveText = this.newSvgElement('text');
-        positiveText.setAttribute('x', positiveTextPositionX);
-        positiveText.setAttribute('y', positiveTextPositionY);
-        positiveText.setAttribute('font-size', textSize);
-        positiveText.setAttribute('font-weight', 'bold');
-        positiveText.setAttribute('fill', 'red');
-        positiveText.innerHTML = '+';
+        var positiveText = this.svgText(positiveTextPositionX, positiveTextPositionY, textSize, '+', 'red');
         
-        var negativeTextPositionX = startX - gaugeWidth;
-        var negativeTextPositionY = outerArcBottom - (textSize / 4 ); //Y-position is the bottom of the text
+        var negativeTextPositionX = positiveTextPositionX;
+        var negativeTextPositionY = outerArcBottom - (textSize / 2);
             
-        var negativeText = this.newSvgElement('text');
-        negativeText.setAttribute('x', negativeTextPositionX);
-        negativeText.setAttribute('y', negativeTextPositionY);
-        negativeText.setAttribute('font-size', textSize);
-        negativeText.setAttribute('font-weight', 'bold');
-        negativeText.setAttribute('fill', 'red');
-        
-        negativeText.innerHTML = '&ndash;';
-        
-        var zeroText = this.newSvgElement('text');
-        zeroText.setAttribute('x', halfwayX + gaugeWidth);
-        zeroText.setAttribute('y', halfwayY + (textSize / 4));
-        zeroText.setAttribute('font-size', textSize);
-        zeroText.setAttribute('font-weight', 'bold');
-        zeroText.setAttribute('font-family', 'arial');
-        zeroText.setAttribute('fill', 'green');
-        zeroText.innerHTML = '0';
-        
-        
+        var negativeText = this.svgText(negativeTextPositionX, negativeTextPositionY, textSize, '&ndash;', 'red');
+        var zeroText = this.svgText(halfwayX + gaugeWidth, halfwayY, textSize, '0', 'green');
         
         
         var percentageDifference = conf.percentageDifference; // Exactly halfway in the top section -- same as in testing
         
+        //Prevent overflow if user passes numbers outside of accepted range
         if(percentageDifference > 100){
             percentageDifference = 100;
         } else if (percentageDifference < -100){
             percentageDifference = -100;
         }
-        
-        console.log('halfwayPoint['+halfwayPoint+']');
         
         var halfwayMarker = this.newSvgElement("path");
         halfwayMarker.setAttribute('d', 'M'+halfwayPoint+' h'+(gaugeWidth/2)+' M'+halfwayPoint+' h-'+gaugeWidth+' Z');
@@ -118,10 +105,11 @@ SVGENIUS.charts = {
         var innerArcYOffset = (radius - gaugeWidth) * Math.sin(angle);
         var innerArcXOffset = (radius - gaugeWidth) * Math.cos(angle);
         
-        console.log('offsets ['+innerArcXOffset+','+innerArcYOffset+'] ['+outerArcXOffset+','+outerArcYOffset+']');
-        
+        //Flags for arc decision making
         var innerArcFlag = percentageDifference > 0 ? 0 : 1;
         var outerArcFlag = percentageDifference > 0 ? 1 : 0;
+        
+        //Rendering negative chart doesn't work without this correction. Can't tell you why...
         var correction = percentageDifference > 0 ? 1 : 0;
         
         var overlay = this.newSvgElement("path");
@@ -129,10 +117,11 @@ SVGENIUS.charts = {
         overlay.setAttribute('fill', (Math.abs(percentageDifference)>conf.threshold?'red':'green'));
         overlay.setAttribute('stroke-width', '0');
         
-        //Add the things to the page
+        //Add the things to the page to create beautiful gauge
         gaugeContainer.appendChild(svg);
         svg.appendChild(positiveText);
         svg.appendChild(negativeText);
+        svg.appendChild(base);
         svg.appendChild(overlay);
         svg.appendChild(outline);
         svg.appendChild(halfwayMarker);
